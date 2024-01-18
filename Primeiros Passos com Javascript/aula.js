6a6234df74e8ec2e1c53c5065ed75b2a024a30d1
Karleandro <143852351+karleandrosilva@users.noreply.github.com>
2024-01-17 22:30:08 -0300

    update

---
+++ Primeiros Passos com Javascript/aula.js
@@ -0,0 +1,26 @@
+const pi = 3.14 //  - > (constante) nao altera o valor da variavel
+
+let variavel = 10; //  - > let altera o valor da variavel
+
+console.log(variavel);
+
+variavel = 15
+
+console.log(variavel);
+
+variavel_2 = variavel + 20; 
+
+console.log(variavel_2);
+
+soma = 10 + 10 // SOMA
+subt = 10 - 10 // SUBTRAÇÃO
+mult = 10 * 10 // MULTIPLICAÇÃO
+divi = 10 / 10 // DIVISÃO 
+
+console.log(soma)
+console.log(subt)
+console.log(mult)
+console.log(divi)
+
+
+
---
+++ Primeiros Passos com Javascript/desafio.js
@@ -0,0 +1,14 @@
+//1 - Preço do combústivel;
+const preco_combustivel = 5.79
+
+//2 - Gasto médio de combústivel do carro por KM;
+let gasto_porKm = 2
+
+//3 - Distância em KM da viagem;
+let distancia = 15
+
+// o valor que será gasto 
+
+let valor = (distancia * gasto_porKm) * preco_combustivel
+
+console.log(valor)
