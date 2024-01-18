4fbeb3d626a1ba82fe2e07173a581f6b830639bc
Karleandro <143852351+karleandrosilva@users.noreply.github.com>
2024-01-18 00:29:18 -0300

    update

--- Primeiros Passos com Javascript/desafio.js
+++ Primeiros Passos com Javascript/desafio.js
@@ -1,14 +1,16 @@
 //1 - Preço do combústivel;
-const preco_combustivel = 5.79
+const preco_combustivel = 5.50
 
 //2 - Gasto médio de combústivel do carro por KM;
-let gasto_porKm = 2
+const gasto_porKm = 1
 
 //3 - Distância em KM da viagem;
-let distancia = 15
+const distancia = 100
+
+// quantos litros consumidos
+const litrosConsumidos = distancia / gasto_porKm 
 
 // o valor que será gasto 
+const valorGasto = litrosConsumidos * preco_combustivel
 
-let valor = (distancia * gasto_porKm) * preco_combustivel
-
-console.log(valor)
+console.log(valorGasto)
